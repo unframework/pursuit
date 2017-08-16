742f59de313f08e744f7fc496121eb4bc8b11a80
Nick Matantsev <nick.matantsev@gmail.com>
2017-08-16 03:08:03 -0400

    Switch to road texture offset and draw asphalt

--- index.js
+++ index.js
@@ -48,7 +48,6 @@ roadCmd = regl({
 
         uniform mat4 camera;
         uniform float drawDistance;
-        uniform float drawOffset;
         attribute vec2 position;
 
         varying vec2 facePosition;
@@ -58,7 +57,7 @@ roadCmd = regl({
 
             gl_Position = camera * vec4(
                 position.x * 4.75,
-                position.y * drawDistance + drawOffset,
+                position.y * drawDistance,
                 0,
                 1.0
             );
@@ -69,21 +68,38 @@ roadCmd = regl({
         precision mediump float;
 
         #pragma glslify: dither = require('glsl-dither/8x8')
+        #pragma glslify: cnoise2 = require('glsl-noise/classic/2d')
 
         uniform float drawDistance;
+        uniform float drawOffset;
 
         varying vec2 facePosition;
 
         void main() {
-            float dist = facePosition.y * drawDistance;
+            vec2 roadPos = vec2(
+                facePosition.x * 4.75,
+                facePosition.y * drawDistance + drawOffset
+            );
+
+            vec2 asphaltPos = roadPos * vec2(20.0, 10.0);
+
+            float wearNoise = cnoise2(roadPos / vec2(4.3, 12.9));
+            float asphaltNoise = cnoise2(vec2(
+                asphaltPos.x - mod(asphaltPos.x, 0.5),
+                asphaltPos.y - mod(asphaltPos.y, 0.5)
+            ));
+
+            float asphaltSpec = clamp((asphaltNoise - 0.8) / 0.2, 0.0, 1.0);
+            float asphaltCrack = clamp(-0.6 - asphaltNoise, 0.0, 1.0) / 0.4;
+
             float lanePos = mod((facePosition.x + 1.0) * 1.6 - 0.1, 1.0);
             float side = 1.0 - step(0.02, lanePos) * step(lanePos, 0.98);
 
-            vec3 color = side * mod(dist, 4.0) > 2.0
+            vec3 color = side * mod(roadPos.y, 4.0) > 2.0
                 ? vec3(0.25, 0.25, 0.27)
-                : vec3(0.16, 0.16, 0.18);
+                : vec3(0.16, 0.16, 0.18) * (1.0 - asphaltCrack * 0.3 + asphaltSpec * 0.8);
 
-            gl_FragColor = vec4(color, 1.0);
+            gl_FragColor = vec4(color * (0.88 + wearNoise * 0.12), 1.0);
 
             float fade = 1.0 - clamp((gl_FragCoord.z - 0.95) / 0.05, 0.0, 1.0);
 
@@ -134,7 +150,7 @@ const timer = new Timer(STEP, 0, function () {
     mat4.rotateX(camera, camera, -Math.PI / 2);
 
     // camera shake and offset
-    vec3.set(cameraPosition, 0.02 * Math.sin(now * 3.43), -offset, -CAMERA_HEIGHT + 0.02 * Math.cos(now * 2.31));
+    vec3.set(cameraPosition, 0.02 * Math.sin(now * 3.43), 0, -CAMERA_HEIGHT + 0.02 * Math.cos(now * 2.31));
     mat4.translate(camera, camera, cameraPosition);
 
     regl.clear({
@@ -142,12 +158,10 @@ const timer = new Timer(STEP, 0, function () {
         depth: 1
     });
 
-    const drawOffset = Math.floor(offset / DRAW_CYCLE) * DRAW_CYCLE;
-
     roadCmd({
         aspectRatio: aspectRatio,
         drawDistance: DRAW_DISTANCE,
-        drawOffset: drawOffset,
+        drawOffset: offset,
         camera: camera
     });
 });
